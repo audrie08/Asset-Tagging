bc6fc40d174db6ed991641d8b63db9d4da438a13
audrie08 <olonaaudreyy@gmail.com>
2025-10-06 13:15:29 +0800

    Update assettagging.py

--- assettagging.py
+++ assettagging.py
@@ -1,1 +1,10 @@
+import streamlit as st
+import pandas as pd
+import gspread
+from google.oauth2.service_account import Credentials
+import warnings
+import io
 
+warnings.filterwarnings('ignore')
+
+st.set_page_config(page_title="BOM Explosion", layout="wide")
